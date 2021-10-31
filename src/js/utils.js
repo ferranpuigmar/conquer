cd5d563ea9c9607e15f7d20108eada2bbfc3804b
iortizfe <iortizfe@uoc.edu>
2021-10-31 08:02:01 +0100

    storage

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,13 +1,26 @@
 export default class LocalStorage {
   localStorage = window.localStorage;
+  sessionStorage = window.sessionStorage;
 
   setLocalStorage(key, data, type){
     const dataToLocaltorage = JSON.stringify(data);
-    this.localStorage.setItem(key, dataToLocaltorage)
+    if(type.includes('session')){
+      this.sessionStorage.setItem(key, dataToLocaltorage)
+    }else{
+
+      this.localStorage.setItem(key, dataToLocaltorage)
+    }
   }
 
-  getLocalStorage(key){
-    const data = this.localStorage.getItem(key);
+  getLocalStorage(key, type){
+    let data;
+
+    if(type.includes('session')){
+        data = this.sessionStorage.getItem(key);
+    }else{
+        data = this.localStorage.getItem(key);
+    }
+    
     return JSON.parse(data);
   }
 }
