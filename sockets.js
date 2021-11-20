bd5b5da0743c8a6649379679aa12cdea4c9a98ce
iortizfe <iortizfe@uoc.edu>
2021-11-20 11:48:54 +0100

    nom

---
+++ sockets.js
@@ -0,0 +1,23 @@
+
+// import { EVENT_TYPES, MESSAGE_TYPES } from "./constants";
+// let players = [];
+
+// const io = require("socket.io")(server);
+
+// const mysockets = () =>{
+//     io.on("connection", (socket) => {
+//         console.log("User connected: " + socket.id);
+      
+//         socket.on("game", (data) => {
+//           socket.emit("game", data);
+//         });
+      
+//         socket.on("room", (data) => {
+//           socket.to(data.roomID).emit("room", data);
+//         });
+//       });
+// }
+
+// export default mysockets;
+
+
