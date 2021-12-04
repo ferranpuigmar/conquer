8a0ebac1535210a36860a0cfb76e8aa3079937e2
Ferran <ferran.puigmar@gmail.com>
2021-12-04 19:22:43 +0100

    improve server.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.render("login");
+});
+
 router.get("/login", (req, res) => {
   res.render("login");
 });
@@ -9,8 +13,17 @@ router.get("/register", (req, res, next) => {
   res.render("register");
 });
 
-router.get("/rooms", (req, res, next) => {
-  res.render("rooms");
+router.get("/rooms", function (req, res) {
+  const data = {
+    outside: false,
+    boxRooms: [
+      { id: "red-room-box" },
+      { id: "blue-room-box" },
+      { id: "green-room-box" },
+      { id: "orange-room-box" },
+    ],
+  };
+  res.render("rooms", data);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 // Configuración inicial
 const express = require("express");
-const {engine} = require('express-handlebars');
+const { engine } = require("express-handlebars");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
@@ -32,54 +32,21 @@ app.use(
 );
 
 // Motor de plantilla
-const hbs = require("hbs");
-//hbs.registerPartials(__dirname + "/src/views/partials", function (err) {});
-
-app.engine('hbs', engine({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  partialsDir: __dirname + '/src/views/partials/'
-}));
-app.set('view engine', 'hbs');
+app.engine(
+  "hbs",
+  engine({
+    defaultLayout: "main",
+    extname: ".hbs",
+    partialsDir: __dirname + "/src/views/partials/",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", __dirname + "/src/views");
 
 app.use(express.static(__dirname + "/public"));
 
 const index = require("./routes/index");
-//app.use("/", index);
-app.get("/", function (req, res) {
-  const data = { 
-    outside: true
-  }
-  res.render("login", data);
-});
-
-app.get("/login", function (req, res) {
-  const data = { 
-    outside: true
-  }
-  res.render("login",data);
-});
-
-app.get("/register", function (req, res) {
-  const data = { 
-    outside: true
-  }
-  res.render("register", data);
-});
-
-app.get("/rooms", function (req, res) {
-  const data = { 
-    outside: false,
-    boxRooms: [
-      {id: 'red-room-box'},
-      {id: 'blue-room-box'},
-      {id: 'green-room-box'},
-      {id: 'orange-room-box'}
-    ]
-  }
-  res.render("rooms", data);
-});
+app.use("/", index);
 
 // Iniciar servidor
 http.listen(port, () => {
